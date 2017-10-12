d34399ad3b9791284f75cafba0e9d9af2e707877
mariomaldonado007 <mariomaldonado007@hotmail.com>
2017-10-11 23:06:22 -0500

    subir las instrucciones

---
+++ git.py
@@ -0,0 +1,19 @@
+intructions = """
+
+inicializar repositorio:
+git init
+
+añadir archivos
+git add .
+
+crear commits: 
+git commit -m "mensaje de comit"
+
+subir los cambios:
+git push
+"""
+
+print (intructions)
+
+
+
